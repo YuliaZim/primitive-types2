48ff8717a0ff2c44ccac72817ccc7271bef2af65
Yulia Zimina <yxalzova@gmail.com>
2025-06-03 15:44:39 +0400

    primitive

---
+++ src/main.java
@@ -0,0 +1,20 @@
+public class main {
+    public static void main(String[] args) {
+
+        int balance = 200;
+        int replenishment = 1
+        600;
+
+        int bonus;
+        if (replenishment>1000) {
+            bonus = (replenishment / 100);
+        } else {
+            bonus = 0;
+        }
+
+        int result = (balance + replenishment + bonus);
+
+        System.out.println("Итоговый баланс:" + result);
+    }
+}
+
