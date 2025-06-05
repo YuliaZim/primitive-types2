38fb60ddde4e1ff822810808e4c4689b1939ad4f
Yulia Zimina <yxalzova@gmail.com>
2025-06-05 19:54:03 +0400

    correction

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
 
         int balance = 200;
-        int replenishment = 1
-        600;
+        int replenishment = 1600;
 
         int bonus;
-        if (replenishment>1000) {
+        if (replenishment > 1000) {
             bonus = (replenishment / 100);
         } else {
             bonus = 0;
